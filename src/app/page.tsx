5d3b6d6b24ef4662224d72bbc874da5a76782bcd
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2024-01-19 13:30:41 +0100

    add at least one cyppress test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1>Runroom - NextJS Archetype</h1>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
--- tests/cypress/support/index.d.ts
+++ tests/cypress/support/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 declare namespace Cypress {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
   interface Chainable<Subject = any> {
     userLogin(): Chainable<any>;
     adminLogin(): Chainable<any>;
