07894c93e58c6233390f87b010eee110c568dc31
Adrià Marcet <amarcet@runroom.com>
2024-02-05 12:05:11 +0100

    Create Welcome component and use it

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,21 +1,13 @@
 'use client';
-import styles from '@/styles/Welcome.module.css';
 
 import { LanguageSwitcher } from '@/Shared/ui/components/LanguageSwitcher';
-
-import { useTranslation } from 'react-i18next';
+import { Welcome } from '@/Shared/ui/components/Welcome';
 
 export default function Home() {
-  const { t } = useTranslation('common');
   return (
-    <main className={styles.welcome}>
-      <h1 className={styles.welcome__title}>
-        <span className={styles.welcome__gradient}>Runroom Starter</span>
-        <span className={`${styles.welcome__gradient} ${styles.welcome__below}`}>
-          {t(`welcome`)}
-        </span>
-      </h1>
+    <>
+      <Welcome />
       <LanguageSwitcher />
-    </main>
+    </>
   );
 }
